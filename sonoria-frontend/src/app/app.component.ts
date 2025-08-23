1053488b98f0953cb83ab89bfb9959cca1b92c26
hhaoz <hao.tt03643@sinhvien.hoasen.edu.vn>
2025-08-23 14:05:46 +0700

    init project

---
+++ sonoria-frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {HeaderComponent} from './components/header/header.component';
+import {PlayerBarComponent} from './components/player-bar/player-bar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent, PlayerBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'sonoria-frontend';
+}
---
+++ sonoria-frontend/src/app/components/music-bar/music-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-bar',
+  imports: [],
+  templateUrl: './music-bar.component.html',
+  styleUrl: './music-bar.component.scss'
+})
+export class MusicBarComponent {
+
+}
