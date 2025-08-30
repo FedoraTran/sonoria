84148fcaafde9c0a8906d10c33ce288fc1cf1488
LamQuangMinh2005 <minh.lq02519@sinhvien.hoasen.edu.vn>
2025-08-30 13:53:57 +0700

    Thêm pages/home và player-bar

--- sonoria-frontend/src/app/pages/home/home.component.ts
+++ sonoria-frontend/src/app/pages/home/home.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  scrollExplore(dir: number) {
+  const el = document.querySelector('.explore-list');
+  if (el) {
+    if (dir > 0) {
+      el.scrollTo({ left: el.scrollWidth, behavior: 'smooth' });
+    } else {
+      el.scrollTo({ left: 0, behavior: 'smooth' });
+    }
+  }
+}
 
+  scrollTrending(dir: number) {
+  const el = document.querySelector('.trending-list');
+  if (el) {
+    if (dir > 0) {
+      el.scrollTo({ left: el.scrollWidth, behavior: 'smooth' });
+    } else {
+      el.scrollTo({ left: 0, behavior: 'smooth' });
+    }
+  }
+
+}
 }
